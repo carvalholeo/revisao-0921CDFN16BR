9b1d3282ab936cf016cff5b610cb289c4ca2605e
Léo Carvalho <carvalho.csleo@gmail.com>
2022-01-20 21:03:36 -0300

    Troca função nomeada para arrow function

--- callbacks.js
+++ callbacks.js
@@ -1,26 +1,12 @@
 function calculadora (operador, operandoA, operandoB) {
   console.log(operador(operandoA, operandoB))
 }
-
 //operador com arrow function
 const soma = (numeroA, numeroB) => numeroA + numeroB;
+const subtrair = (numeroA, numeroB) => numeroA - numeroB;
+const dividir = (numeroA, numeroB) => numeroB === 0 ? NaN : numeroA / numeroB;
+const multiplicar = (numeroA, numeroB) => numeroA * numeroB;
+const potencia = (numeroA, numeroB) => numeroA ** numeroB;
 
-// operador com funções nomeadas
-function subtrair(numeroA, numeroB) {
-  return numeroA - numeroB;
-}
-function dividir(numeroA, numeroB) {
-  if (numeroB === 0) {
-    return NaN;
-  }
-  return numeroA / numeroB;
-}
-function multiplicar (numeroA, numeroB) {
-  return numeroA * numeroB
-}
-function potencia (numeroA, numeroB) {
-  return numeroA ** numeroB
-}
-
-calculadora(subtrair, 2, 0);
+calculadora(dividir, 2, 2);
 
