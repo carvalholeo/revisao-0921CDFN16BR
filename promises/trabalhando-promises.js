d4dac2be6edb2fe5799f1cd125993e48d5e6eb92
Léo Carvalho <carvalho.csleo@gmail.com>
2022-02-08 21:45:06 -0300

    Troca estrutura de .then/.catch para try/catch

--- promises/trabalhando-promises.js
+++ promises/trabalhando-promises.js
@@ -1,12 +1,24 @@
 const universidade = require('./promises')
 
 console.log('antes da promise')
-console.log(universidade(47))
-universidade(47)
-  .then((dados) => {
-    console.log(dados)
-  })
-  .catch((erro) => {
-    console.error(erro)
-  })
+
+// universidade(47)
+//   .then((dados) => {
+//     console.log(dados)
+//   })
+//   .catch((erro) => {
+//     console.error(erro)
+//   })
+
+async function executada() {
+  try {
+    const resposta = await universidade(11);
+    console.log(resposta);
+  } catch (batata_murcha) {
+    console.trace(batata_murcha)
+  }
+}
+
+executada()
+
 console.log('depois da promise')
